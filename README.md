d7730149dddac579d38e0817c786e3470448018b
Dariusz Wietecha <wietecha.dariusz@gmail.com>
2020-09-08 00:49:24 +0200

    Configuration

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
-Nest.js Readme archived as `README nestjs.md`
+# Encrypted at rest API
 
-logs.service.ts unit tests coverage was realized by unit tests of other modules.
+## Introduction
+
+## Configuration
+DATABASE_USER=
+DATABASE_NAME=
+DATABASE_PASSWORD=
+IGNORE_ENV_FILE
+
+
+## Endpoints
+
+## Notes
+* Nest.js Readme archived as `README nestjs.md`
+* logs.service.ts unit tests coverage was realized by unit tests of other modules.
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DataModule } from './data/data.module';
 import { LogsModule } from './logs/logs.module';
@@ -7,16 +8,29 @@ import { CryptoModule } from './crypto/crypto.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      ignoreEnvFile: process.env.IGNORE_ENV_FILE as unknown as boolean,
+    }),
     DataModule,
     LogsModule,
-    TypeOrmModule.forRoot({
-      "type": "mysql",
-      "host": "localhost",
-      "port": 3306,
-      "username": "root",
-      "database": "nestjs-aes-crypto",
-      "autoLoadEntities": true,
-      "synchronize": true
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        const passwordObject = configService.get('DATABASE_PASSWORD') ?
+          { password: configService.get('DATABASE_PASSWORD') } : {};
+
+        return {
+          ...passwordObject,
+          "type": "mysql",
+          "host": "localhost",
+          "port": 3306,
+          "username": configService.get('DATABASE_USER'),
+          "database": configService.get('DATABASE_NAME'),
+          "autoLoadEntities": true,
+          "synchronize": true
+        }
+      },
+      inject: [ConfigService],
     }),
     CryptoModule,
   ],
