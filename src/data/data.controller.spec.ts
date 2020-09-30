3db28b183aa6df32c2bd02504a9b314e560e0bc4
Dariusz Wietecha <darek@techtailor.co>
2020-09-30 08:17:04 +0200

    Husky-pretty-quick-staged

--- src/data/data.controller.spec.ts
+++ src/data/data.controller.spec.ts
@@ -13,10 +13,7 @@ describe('DataController', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [
-        mockDataRepository,
-        mockLogsRepository,
-      ],
+      imports: [mockDataRepository, mockLogsRepository],
       controllers: [DataController],
       providers: [
         CryptoService,
@@ -30,7 +27,7 @@ describe('DataController', () => {
           provide: getRepositoryToken(Log),
           useValue: mockLogsRepository(),
         },
-      ]
+      ],
     }).compile();
 
     dataController = module.get<DataController>(DataController);
@@ -38,35 +35,47 @@ describe('DataController', () => {
 
   describe('getById', () => {
     it('standard data', async () => {
-      const result = await dataController.getById(mockRequest, 'testId', 'qwerqewr12341');
-      expect(result).toEqual([{
-        id: 'testId',
-        value: { 'test': 'test' },
-      }]);
+      const result = await dataController.getById(
+        mockRequest,
+        'testId',
+        'qwerqewr12341',
+      );
+      expect(result).toEqual([
+        {
+          id: 'testId',
+          value: { test: 'test' },
+        },
+      ]);
     });
 
     it('data with id="*"', async () => {
-      const result = await dataController.getById(mockRequest, '*', 'qwerqewr12341');
+      const result = await dataController.getById(
+        mockRequest,
+        '*',
+        'qwerqewr12341',
+      );
       expect(result).toEqual([]);
     });
 
     it('Decryption Error', async () => {
-      const result = await dataController.getById(mockRequest, 'testId', 'qwerqewr1234');
+      const result = await dataController.getById(
+        mockRequest,
+        'testId',
+        'qwerqewr1234',
+      );
       expect(result).toEqual([]);
     });
   });
 
   it('update', async () => {
-    const result = await dataController.update(
-      'testId',
-      'qwerqewr12341',
-      {
-        'value': {
-          'test': 'test'
-        }
+    const result = await dataController.update('testId', 'qwerqewr12341', {
+      value: {
+        test: 'test',
       },
-    );
+    });
     expect(result.id).toEqual('testId');
-    expect(result.value).toEqual('U2FsdGVkX19EfAwVXVTfK6pn/6tZdvKIW6ZLfHKaitA=');
+    expect(result.value).toEqual(
+      'U2FsdGVkX19EfAwVXVTfK6pn/6tZdvKIW6ZLfHKaitA=',
+    );
   });
 });
